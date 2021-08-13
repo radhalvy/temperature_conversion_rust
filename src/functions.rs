33f879c5741c76bce4c9911ec9af66ca2ac7a6c3
Radhalvy Bernabel <radhalvybernabel@gmail.com>
2021-08-12 21:55:35 -0400

    Added a new conversion type to the function file and updated the main file

--- src/functions.rs
+++ src/functions.rs
@@ -5,7 +5,7 @@ pub fn conversion_type() -> u8 {
     
     let mut user_answer = String::new();
 
-    println!("What do you want to convert to:\nCelsius to fahrenheit (1) or fahrenheit to celsius (2)?");
+    println!("What do you want to convert to:\nCelsius to Fahrenheit (1) \tFahrenheit to Celsius (2) \tCelsius to Kelvin? (3)");
     io::stdin()
         .read_line(&mut user_answer)
         .expect("Failed to read line");
@@ -23,7 +23,7 @@ pub fn user_celsius_value() -> f64 {
     
     let mut user_celsius = String::new();
     
-    println!("\nEnter the temperature that you want to convert to fahrenheit:");
+    println!("\nEnter the temperature in celsius:");
     io::stdin()
         .read_line(&mut user_celsius)
         .expect("Failed to read line");
@@ -41,7 +41,7 @@ pub fn user_fahrenheit_value() -> f64 {
 
     let mut user_fahrenheit = String::new();
 
-    println!("\nEnter the temperature that you want to convert to celsius:");
+    println!("\nEnter the temperature in fahrenheit:");
     io::stdin()
         .read_line(&mut user_fahrenheit)
         .expect("Failed to read line");
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ fn main() {
         // If true, makes the conversion from celsius to fahrenheit
         1 => {
             let final_conversion = functions::user_celsius_value() * 1.8 + 32 as f64;
-            println!("\nThe temperature in fahrenheit is {:.1}\n", final_conversion);
+            println!("\nThe temperature in fahrenheit is {:.2}\n", final_conversion);
         }
         // If true, makes the conversion from fahrenheit to celsius
         2 => {
             let final_conversion = (functions::user_fahrenheit_value() - 32 as f64) / 1.8;
-            println!("\nThe temperature in celsius is {:.1}", final_conversion);
+            println!("\nThe temperature in celsius is {:.2}", final_conversion);
+        }
+        3 => {
+            let final_conversion = functions::user_celsius_value() + 273.15;
+            println!("\nThe temperature in kelvin is {:.2}", final_conversion);
         }
         _ => {
             println!("Invalid option");
